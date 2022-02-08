72c7ed01a92de535c9e137869cdaf333e4e3d0bb
dtmcdona <davidmcdwork@gmail.com>
2022-02-07 20:23:31 -0700

    Updated app.py GUI

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
@@ -7,23 +7,49 @@ from kivy.uix.textinput import TextInput
 
 class DMAutomate(App):
     def build(self):
-        self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.8, 0.8)
-        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
-        self.window.add_widget(Image(source="logo.png"))
-        self.title = Label(text="DMAutomate")
-        self.window.add_widget(self.title)
-        self.status = Label(text="Standby")
-        self.window.add_widget(self.status)
-        self.record = Button(text="Start Recording")
+        self.is_recording = False
+        self.window = BoxLayout(orientation ='vertical')
+        self.toolbar = BoxLayout(orientation ='horizontal', size_hint = (1, .1))
+        self.statusbar = BoxLayout(orientation ='horizontal', size_hint = (1, .1))
+        self.bodysection = BoxLayout(orientation='vertical')
+        self.record = Button(text="Record")
+        self.create = Button(text="Create")
+        self.open = Button(text="Open")
+        self.save = Button(text="Save")
+        self.delete = Button(text="Delete")
+        self.appsettings = Button(text="Settings")
+        self.play = Button(text="Play", size_hint = (.2, 1))
+        self.currentfile = "dm_macro.py"
+        self.status = "Standby"
+        self.duration = ""
+        self.statusbartext = Button(text="Current file: " + self.currentfile +
+                                         " - Status: " + self.status +
+                                         " - Duration: " + self.duration)
         self.record.bind(on_press=self.record_callback)
-        self.window.add_widget(self.record)
+        self.toolbar.add_widget(self.record)
+        self.toolbar.add_widget(self.create)
+        self.toolbar.add_widget(self.open)
+        self.toolbar.add_widget(self.delete)
+        self.toolbar.add_widget(self.save)
+        self.toolbar.add_widget(self.appsettings)
+        self.statusbar.add_widget(self.play)
+        self.statusbar.add_widget(self.statusbartext)
+        self.window.add_widget(self.toolbar)
+        self.window.add_widget(self.statusbar)
+        self.window.add_widget(self.bodysection)
 
         return self.window
 
     def record_callback(self, instance):
-        self.status.text = "Recording"
+        self.is_recording = False if self.is_recording else True
+        self.record.text = "Stop" if self.is_recording else"Record"
+        self.status = "Recording" if self.is_recording else "Standby"
+        self.update_status_bar()
+
+    def update_status_bar(self):
+        self.statusbartext.text = "Current file: " + self.currentfile + \
+                                    " - Status: " + self.status + \
+                                    " - Duration: " + self.duration
 
 if __name__ == "__main__":
     DMAutomate().run()
